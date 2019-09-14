0cff59aaff43ff6cfbbef4a453cb8ac6b2aeaf54
partofgreaf <partofgreaf@gmail.com>
2019-09-14 15:57:28 +0300

    callback modals node, smtp send to mail

--- js/script.js
+++ js/script.js
@@ -13,14 +13,73 @@ $(window).scroll(function() {
     });
   }
 });
+sendMail = (title, body, cb) => {
+  Email.send({
+    SecureToken: "f33c7167-29a8-410c-aceb-fd914a44284d",
+    To: "dimasik1502@mail.ru",
+    From: "dimasik.komarov.150298@gmail.com",
+    Subject: title,
+    Body: body
+  }).then(message => {
+    if (message === "OK") {
+      cb && cb();
+    }
+  });
+};
 $(document).ready(() => {
-  const mask = IMask(document.getElementById("input-phone"), {
+  $("#callback-button").click(() => {
+    const name = $("#input-name").val();
+    const phone = $("#input-callback-phone").val();
+    if (name && phone && $("#checkbox-call").prop("checked")) {
+      sendMail(
+        "Обратный звонок",
+        `<p><span><b>Имя: </b>${name}</span></p><p><span><b>Телефон: </b>${phone}</span></p>`,
+        () => {
+          $("#input-name").val("");
+          $("#input-callback-phone").val("");
+          $("#callback-modal").modal("hide");
+        }
+      );
+    } else {
+      $("#callback-button").notify("Заполните все поля", {
+        position: "top center"
+      });
+    }
+    //sendMail()
+  });
+  $('#sign-btn').click(()=>{
+    const name = $("#sign-input").val();
+    const phone = $("#sign-phone").val();
+    if (name && phone && $("#checkbox").prop("checked")) {
+      sendMail(
+        "Записаться на занятие",
+        `<p><span><b>Имя: </b>${name}</span></p><p><span><b>Телефон: </b>${phone}</span></p>`,
+        () => {
+          $("#sign-input").val("");
+          $("#sign-phone").val("");
+          $("#sign-up-modal").modal("hide");
+        }
+      );
+    } else {
+      $("#sign-btn").notify("Заполните все поля", {
+        position: "top center"
+      });
+    }
+  });
+  IMask(document.getElementById("sign-phone"), {
+    mask: "+{7}(000)000-00-00"
+  });
+  IMask(document.getElementById("input-callback-phone"), {
     mask: "+{7}(000)000-00-00"
   });
   const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 300
   });
+
   $("#up-button").click(() => {
+    // f33c7167-29a8-410c-aceb-fd914a44284d
+    //a5ff9ffa-626d-4ad6-a462-3138a459343d
+
     scroll.animateScroll(0);
   });
 
@@ -37,18 +96,18 @@ $(document).ready(() => {
     $("#slide-panel").toggleClass("open");
   });
 
-  // var feed = new Instafeed({
-  //   accessToken: "445632943.c4d20dc.379c66e111a44c7d9a3f2e3c2ca4907b",
-  //   clientId: "c4d20dcb5a1240ce8f543a48a0e969b4",
-  //   get: "user",
-  //   addClass: "gallery-img",
-  //   userId: "8535532406",
-  //   resolution: "low_resolution",
-  //   template:
-  //     '<div class="col-3"><div class="instagram-image-wraper">{{likes}}<a class="test" href="{{link}}"><img src="{{image}}" /></a></div></div>',
-  //   limit: 12,
-  //   target: "inst"
-  // });
-  // feed.run();
+  var feed = new Instafeed({
+    accessToken: "445632943.1677ed0.7044278c01a24a308024ed330f8b0337",
+    clientId: "c4d20dcb5a1240ce8f543a48a0e969b4",
+    get: "user",
+    addClass: "gallery-img",
+    userId: "8535532406",
+    resolution: "low_resolution",
+    template:
+      '<div class="col-3"><div class="instagram-image-wraper">{{likes}}<a class="test" href="{{link}}"><img src="{{image}}" /></a></div></div>',
+    limit: 12,
+    target: "inst"
+  });
+  feed.run();
   // console.log(feed);
 });
